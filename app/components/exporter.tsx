bfe23b35ce8e3f069a2c6341979a11f9331524bc
RoyZ <RoyZ_@outlook.com>
2023-12-19 15:48:11 +0800

    [DMR] AI Center

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -530,9 +530,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>[DMR] AI Center</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              github.com/RoyZ-iwnl/ChatGPT-Next-Web
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
